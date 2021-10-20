699697cdf36bed03b6467fba6008b83732204e0e
AsilM <asil9802mum@gmail.com>
2021-10-21 00:08:51 +0500

    Updates

---
+++ precache-manifest.49237853498ecbd893e106b77f75076c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46aea05a5c67611a9976eeef17735253",
+    "url": "/lesson-27/index.html"
+  },
+  {
+    "revision": "b2dc98e85d5c835f30b1",
+    "url": "/lesson-27/static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "da70af40d58e27fb2c9f",
+    "url": "/lesson-27/static/js/2.bacdefbd.chunk.js"
+  },
+  {
+    "revision": "d78499fd0e576c7dccb1",
+    "url": "/lesson-27/static/js/3.8c57efa6.chunk.js"
+  },
+  {
+    "revision": "b14f6f1c52a9fd51a1fa",
+    "url": "/lesson-27/static/js/4.442d4850.chunk.js"
+  },
+  {
+    "revision": "b37c8797486534c14cf7",
+    "url": "/lesson-27/static/js/5.901c478d.chunk.js"
+  },
+  {
+    "revision": "c153923702838bfb30ce",
+    "url": "/lesson-27/static/js/6.ae9584d5.chunk.js"
+  },
+  {
+    "revision": "de19dcae4d445e7fc220",
+    "url": "/lesson-27/static/js/7.7a974835.chunk.js"
+  },
+  {
+    "revision": "83f06f65906db88268f6",
+    "url": "/lesson-27/static/js/8.7f3daf71.chunk.js"
+  },
+  {
+    "revision": "b2dc98e85d5c835f30b1",
+    "url": "/lesson-27/static/js/main.85c83d8b.chunk.js"
+  },
+  {
+    "revision": "ac595c7d3c27c81af92e",
+    "url": "/lesson-27/static/js/runtime~main.96cd8e94.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/lesson-27/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/lesson-27/static/media/shopping-bag.30f07071.svg"
+  }
+]);
